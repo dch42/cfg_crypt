f19c572dfac391451614614db330a2999ae1cfc4
ddch42 <85430824+dch42@users.noreply.github.com>
2022-01-02 01:45:30 -0600

    write to user $HOME

--- cfg_crypt.py
+++ cfg_crypt.py
@@ -5,18 +5,29 @@ import getpass
 import os.path
 import ast
 from cryptography.fernet import Fernet
+from pathlib import Path
+import pathlib
+
+home_dir = Path.home()
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+
+def make_hidden_dir(script_name):
+    """Make hidden directory with script name in user $HOME"""
+    hidden_path = pathlib.Path(f'{home_dir}/.{script_name}')
+    hidden_path.mkdir(parents=True, exist_ok=True)
+    return hidden_path
 
 def gen_key():
     """Generate fernet encryption key"""
     print("Generating encryption key...")
     fernet_key = Fernet.generate_key()
-    with open('.cfg.key', 'wb') as cfg_key:
+    with open(f'{hidden_path}/.{script_name}.key', 'wb') as cfg_key:
         cfg_key.write(fernet_key)
-    print("\033[5m==>\033[0m Key generated at ./.cfg.key\n")
+    print(f"\033[5m==>\033[0m Key generated at '{hidden_path}/.{script_name}.key'\n")
 
 def read_key():
     """Read encryption key"""
-    with open('.cfg.key', 'rb') as filekey:
+    with open(f'{hidden_path}/.{script_name}.key', 'rb') as filekey:
         raw_key = filekey.read()
         fernet_key = Fernet(raw_key)
     return fernet_key
@@ -30,18 +41,18 @@ def gen_cfg():
     return login_info
 
 def encrypt_cfg(login_info, fernet_key):
-    """Encrypt and replace raw cfg file"""
+    """Encrypt and save cfg file"""
     print("\nEncrypting config file...")
     encrypted = fernet_key.encrypt(bytes(login_info, 'utf-8'))
-    with open('.crypt_cfg', 'wb') as encrypted_file:
+    with open(f'{hidden_path}/.{script_name}_cfg', 'wb') as encrypted_file:
         encrypted_file.write(encrypted)
     print('Encryption complete.')
-    print("\033[5m==>\033[0m Encrypted file saved at ./.crypt_cfg\n")
+    print(f"\033[5m==>\033[0m Encrypted file saved at '{hidden_path}/.{script_name}_cfg'\n")
 
 
 def decrypt_cfg(fernet_key):
     """Decrypt bytes and convert to cfg dict"""
-    with open('.crypt_cfg', 'rb') as file:
+    with open(f'{hidden_path}/.{script_name}_cfg', 'rb') as file:
         encrypted = file.read()
     decrypted = fernet_key.decrypt(encrypted)
     decrypted_str = decrypted.decode('utf-8')
@@ -49,10 +60,11 @@ def decrypt_cfg(fernet_key):
     return config_dic
 
 if __name__ == '__main__':
-    if not os.path.exists('.cfg.key'):
+    hidden_path = make_hidden_dir(script_name)
+    if not os.path.exists(f'{hidden_path}/.{script_name}.key'):
         print("No key detected...")
         gen_key()
-    if not os.path.exists('.crypt_cfg'):
+    if not os.path.exists(f'{hidden_path}/.{script_name}_cfg'):
         login_info = gen_cfg()
         key = read_key()
         encrypt_cfg(login_info, key)
